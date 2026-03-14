a349f091cb9293df4baa036134bb419d3c0a0145
FIRDAUS MANGKONA <49714174+Dackin@users.noreply.github.com>
2026-03-15 01:16:52 +0700

    Mengganti fungsi alarm ke mode fungsi set jam
    Refactor digital clock code to improve functionality and add new display modes.

--- digitalclock2.ino
+++ digitalclock2.ino
@@ -6,13 +6,11 @@
 #include <MD_MAX72xx.h>
 #include <SPI.h>
 
-//FONT
-//#include <myFont.h>
-
-//ROTARY ENCODER
+// PIN DEFINITIONS
 #define ENCODER_CLK 4
 #define ENCODER_DT 5
 #define ENCODER_BTN 3
+#define BUZZER_PIN 6 // Pin Buzzer diaktifkan kembali
 
 //MD Parolla MAX7219
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
@@ -24,418 +22,236 @@ MD_Parola layarMAX = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 //DS3231
 DS3231 RTC;
 
-//VARIABLES ENCODER
+//VARIABLES ENCODER & BUTTON
 int encoder = 0;
 int lastCLK = HIGH;
 int currentCLK = HIGH;
 
 unsigned long holdAwal = 0;
-const long holdBatas = 1000;
+const long holdBatas = 1500; 
 
 int buttonTerakhir = HIGH;
 bool holdButton = false;
 
-bool onoff = false;
-bool serialDebug = false; // Setting untuk enable/disable serial debug
+byte seconds, hours;
 
-byte seconds, hours, jamAlarm = 12, menitAlarm = 0;
-String waktu = "0:00";
+// VARIABLE SETTING WAKTU
+int tempJam = 0;
+int tempMenit = 0;
+bool confirmSave = false; 
+
+char textBuffer[15];
+unsigned long lastEncoderReadTime = 0;
+
+int sensitivitasEncoder = 15; 
 
 // ENUM untuk mode display
 enum DisplayMode {
   displayJam,
-  displayTgl,
-  displayAlarm,
-  displayAlarmJam,
-  displayAlarmMenit,
-  displayAlarmOn,
-  displaySetting
+  displaySetJam,    
+  displaySetMenit,  
+  displaySetConfirm 
 };
+
 DisplayMode display = displayJam;
 
 //FUNCTIONS
 void jam() {
-  if (RTC.getHours() == 12 && RTC.getMeridiem() == HOUR_AM) {
-    hours = 0;
-  } else {
-    hours = RTC.getHours();
-  }
+  hours = RTC.getHours(); // Format 24 Jam
 }
 
 String titikdua() {
-  if (RTC.getSeconds() % 2 == 0) {
-    return ":";
-  } else {
-    return " ";
-  }
-}
-
-String ampm() {
-  if (RTC.getMeridiem() == HOUR_AM) {
-    return "AM";
-  } else {
-    return "PM";
-  }
+  if (RTC.getSeconds() % 2 == 0) return ":";
+  else return " ";
 }
 
 void tampilkanLayar() {
   unsigned long sekarang = millis();
   static unsigned long lastUpdate = 0;
 
+  static int lastJam = -1, lastMenit = -1;
+  static bool lastConfirm = !confirmSave; 
+  static DisplayMode lastMode = displayJam;
+
+  bool modeBerubah = (display != lastMode);
+  if (modeBerubah) {
+    layarMAX.displayClear(); 
+    lastMode = display;
+    lastUpdate = 0;          
+  }
+
   switch (display) {
     case displayJam:
       if (sekarang - lastUpdate >= 500) {
-        jam(); // update hours
-        waktu = String() + (hours < 10 ? " " : "") + hours
+        jam();
+        String waktuBaru = String() + (hours < 10 ? " " : "") + hours
                 + titikdua()
                 + (RTC.getMinutes() < 10 ? "0" : "") + RTC.getMinutes();
-
-        layarMAX.displayClear();
-        layarMAX.displayText(
-          waktu.c_str(),
-          PA_CENTER,
-          50,
-          0,
-          PA_PRINT,
-          PA_NO_EFFECT
-        );
-
-        if (serialDebug) {
-          Serial.println("JAM: " + waktu + " " + ampm());
-        }
+                
+        waktuBaru.toCharArray(textBuffer, 15); 
+        layarMAX.displayText(textBuffer, PA_CENTER, 50, 0, PA_PRINT, PA_NO_EFFECT);
         lastUpdate = sekarang;
       }
       break;
 
-    case displayTgl:
-      if (sekarang - lastUpdate >= 1000) {
-        String tanggal = String(RTC.getDay()) + "/"
-                       + String(RTC.getMonth()) + "/"
-                       + String(RTC.getYear());
-
-        layarMAX.displayClear();
-        layarMAX.displayText(
-          tanggal.c_str(),
-          PA_CENTER,
-          50,
-          0,
-          PA_PRINT,
-          PA_NO_EFFECT
-        );
-
-        if (serialDebug) {
-          Serial.println("TGL: " + tanggal);
-        }
-        lastUpdate = sekarang;
+    case displaySetJam:
+      if (modeBerubah || tempJam != lastJam) { 
+        sprintf(textBuffer, "H:%02d", tempJam); // Diubah menjadi H (Hour)
+        layarMAX.displayText(textBuffer, PA_CENTER, 50, 0, PA_PRINT, PA_NO_EFFECT);
+        lastJam = tempJam;
       }
       break;
 
-    case displayAlarm:
-      // tampilkan status alarm
-      layarMAX.displayClear();
-      layarMAX.displayText(
-        "ALARM",
-        PA_CENTER,
-        50,
-        0,
-        PA_PRINT,
-        PA_NO_EFFECT
-      );
-      break;
-
-    case displayAlarmJam:
-      // tampilkan jam alarm
-      {
-        String jamText = "J:" + String(jamAlarm);
-        layarMAX.displayClear();
-        layarMAX.displayText(
-          jamText.c_str(),
-          PA_CENTER,
-          50,
-          0,
-          PA_PRINT,
-          PA_NO_EFFECT
-        );
+    case displaySetMenit:
+      if (modeBerubah || tempMenit != lastMenit) {
+        sprintf(textBuffer, "M:%02d", tempMenit); // Tetap M (Minute)
+        layarMAX.displayText(textBuffer, PA_CENTER, 50, 0, PA_PRINT, PA_NO_EFFECT);
+        lastMenit = tempMenit;
       }
       break;
 
-    case displayAlarmMenit:
-      // tampilkan menit alarm
-      {
-        String menitText = "M:" + String(menitAlarm);
-        layarMAX.displayClear();
-        layarMAX.displayText(
-          menitText.c_str(),
-          PA_CENTER,
-          50,
-          0,
-          PA_PRINT,
-          PA_NO_EFFECT
-        );
+    case displaySetConfirm:
+      if (modeBerubah || confirmSave != lastConfirm) {
+        strcpy(textBuffer, confirmSave ? "SAVE Y" : "SAVE N");
+        layarMAX.displayText(textBuffer, PA_CENTER, 50, 0, PA_PRINT, PA_NO_EFFECT);
+        lastConfirm = confirmSave;
       }
-      break;
-
-    case displayAlarmOn:
-      // tampilkan ON/OFF alarm
-      layarMAX.displayClear();
-      layarMAX.displayText(
-        onoff ? "ON" : "OFF",
-        PA_CENTER,
-        50,
-        0,
-        PA_PRINT,
-        PA_NO_EFFECT
-      );
-      break;
-
-    case displaySetting:
-      // tampilkan tulisan setting
-      layarMAX.displayClear();
-      layarMAX.displayText(
-        "SET",
-        PA_CENTER,
-        50,
-        0,
-        PA_PRINT,
-        PA_NO_EFFECT
-      );
       break;
   }
-
-  // WAJIB untuk refresh display
+  
   layarMAX.displayAnimate();
 }
 
-
 void readButton() {
   unsigned long sekarang = millis();
-  int state = digitalRead(ENCODER_BTN);
+  int stateRaw = digitalRead(ENCODER_BTN); 
 
-  // Deteksi tombol ditekan
-  if (state == LOW && buttonTerakhir == HIGH) {
-    holdAwal = sekarang;   // mulai hitung waktu tekan
-    holdButton = false;    // reset status hold
+  static int state = HIGH;
+  static unsigned long lastDebounceTime = 0;
+  static int lastRawState = HIGH;
+
+  if (stateRaw != lastRawState) {
+    lastDebounceTime = sekarang;
   }
 
-  // Jika tombol sedang ditekan
+  if ((sekarang - lastDebounceTime) > 50) {
+    if (stateRaw != state) {
+      state = stateRaw;
+
+      if (state == LOW) {
+        holdAwal = sekarang;
+        holdButton = false;
+      }
+
+      if (state == HIGH && !holdButton) {
+        // --- LOGIKA CLICK (Siklus Setting) ---
+        if (display == displaySetJam){
+            display = displaySetMenit;
+        }
+        else if (display == displaySetMenit){
+            display = displaySetConfirm;
+        }
+        else if (display == displaySetConfirm){
+            display = displaySetJam;
+        }
+      }
+    }
+  }
+  
+  lastRawState = stateRaw;
+
+  // 2. LOGIKA HOLD (Masuk & Keluar Mode Setting)
   if (state == LOW && !holdButton) {
     if (sekarang - holdAwal > holdBatas) {
-      if (serialDebug) Serial.println(">>> HOLD: Masuk/Keluar Setting <<<");
-      holdButton = true;         // tandai sudah hold
+      holdButton = true; 
 
-      //masuk ke display setalarm
-      if (display == displayJam || display == displayTgl){
-        display = displayAlarmJam;
-        if (serialDebug) Serial.println("Masuk ke setting alarm - Atur JAM");
-
-      //masuk ke setting debug dari display setting
-      } else if (display == displaySetting) {
-        display = displayJam;
-        if (serialDebug) Serial.println("Keluar dari setting");
-        
-      //keluar dari display setalarm
-      } else {
-        // Update alarm RTC sebelum keluar
-        RTC.setAlarm1(jamAlarm, menitAlarm, 0);
-        if (onoff) {
-          RTC.enableAlarm1();
-        } else {
-          RTC.disableAlarm1();
-        }
-        
-        display = displayJam;
-        if (serialDebug) Serial.println("Keluar dari setting alarm");
-      }
-    }
-  }
-
-  // Deteksi tombol dilepas
-  if (state == HIGH && buttonTerakhir == LOW) {
-    if (!holdButton) {
-      if (serialDebug) Serial.println(">>> CLICK: Ganti Display <<<");
-
-      //display normal
-      if (display == displayJam) {
-        display = displayTgl;
-        if (serialDebug) Serial.println("Tampilkan tanggal");
+      if (display == displayJam){
+        display = displaySetJam;
+        tempJam = 0;   
+        tempMenit = 0; 
+        confirmSave = false; 
       } 
-      else if (display == displayTgl) {
-        display = displaySetting;
-        if (serialDebug) Serial.println("Tampilkan setting");
-      }
-      else if (display == displaySetting) {
+      else if (display >= displaySetJam && display <= displaySetConfirm) {
+        if (confirmSave) {
+            RTC.setHours(tempJam);
+            RTC.setMinutes(tempMenit);
+            RTC.setSeconds(0); 
+            
+            // Bunyikan buzzer sebagai notifikasi sukses tersimpan (1500 Hz, 300 ms)
+            tone(BUZZER_PIN, 1500, 300); 
+        }
         display = displayJam;
-        if (serialDebug) Serial.println("Tampilkan jam");
-      }
-
-      //display pas setting alarm
-      else if (display == displayAlarmJam){
-        display = displayAlarmMenit;
-        if (serialDebug) Serial.println("Setting alarm - Atur MENIT");
-      }
-      else if (display == displayAlarmMenit){
-        display = displayAlarmOn;
-        if (serialDebug) Serial.println("Setting alarm - Atur ON/OFF");
-      }
-      else if (display == displayAlarmOn){
-        display = displayAlarmJam;
-        if (serialDebug) Serial.println("Setting alarm - Atur JAM");
       }
     }
   }
-
-  // Simpan state tombol untuk pembacaan berikutnya
-  buttonTerakhir = state;
 }
 
 void readHandle() {
-  // Cek apakah ada perubahan encoder
-  if (encoder == 0){
-    return;
-  }
-
+  if (encoder == 0) return;
+  
   int encoderChange = encoder;
-  encoder = 0; // Reset counter setelah diproses
+  encoder = 0; 
 
   switch (display) {
-    case displayAlarmJam:
-      jamAlarm += encoderChange;
-      // Validasi range jam (0-23)
-      if (jamAlarm > 23) jamAlarm = 0;
-      if (jamAlarm < 0) jamAlarm = 23;
-      
-      if (serialDebug) {
-        Serial.print(">>> JAM ALARM: ");
-        Serial.println(jamAlarm);
-      }
+    case displaySetJam:
+      tempJam += encoderChange;
+      if (tempJam > 23) tempJam = 0; 
+      if (tempJam < 0) tempJam = 23;
       break;
       
-    case displayAlarmMenit:
-      menitAlarm += encoderChange;
-      // Validasi range menit (0-59)
-      if (menitAlarm > 59) menitAlarm = 0;
-      if (menitAlarm < 0) menitAlarm = 59;
-      
-      if (serialDebug) {
-        Serial.print(">>> MENIT ALARM: ");
-        Serial.println(menitAlarm);
-      }
+    case displaySetMenit:
+      tempMenit += encoderChange;
+      if (tempMenit > 59) tempMenit = 0;
+      if (tempMenit < 0) tempMenit = 59;
       break;
       
-    case displayAlarmOn:
-      // Toggle on/off dengan setiap putaran encoder
-      if (encoderChange != 0) {
-        onoff = !onoff;
-        if (serialDebug) {
-          Serial.print(">>> ALARM STATUS: ");
-          Serial.println(onoff ? "ON" : "OFF");
-        }
-      }
+    case displaySetConfirm:
+      if (encoderChange != 0) confirmSave = !confirmSave;
       break;
       
-    case displaySetting:
-      // Toggle serial debug dengan setiap putaran encoder
-      if (encoderChange != 0) {
-        serialDebug = !serialDebug;
-        // Selalu tampilkan perubahan setting ini
-        Serial.print(">>> SERIAL DEBUG: ");
-        Serial.println(serialDebug ? "ON" : "OFF");
-      }
+    default:
       break;
   }
 }
 
-// Fungsi polling encoder - Yang sudah bekerja!
 void readEncoderPolling() {
   currentCLK = digitalRead(ENCODER_CLK);
-  
-  // Jika CLK berubah dari HIGH ke LOW (falling edge)
   if (currentCLK != lastCLK && currentCLK == LOW) {
-    int dtValue = digitalRead(ENCODER_DT);
-    
-    if (dtValue == LOW) {
-      encoder++;
-      // Hanya tampilkan debug jika serial debug aktif dan sedang setting
-      if (serialDebug && (display >= displayAlarmJam && display <= displaySetting)) {
-        Serial.println(">>> ENCODER: CLOCKWISE <<<");
-      }
-    } else {
-      encoder--;
-      // Hanya tampilkan debug jika serial debug aktif dan sedang setting
-      if (serialDebug && (display >= displayAlarmJam && display <= displaySetting)) {
-        Serial.println(">>> ENCODER: COUNTER-CLOCKWISE <<<");
+    unsigned long sekarang = millis();
+    if (sekarang - lastEncoderReadTime > sensitivitasEncoder) {
+      delay(1);
+      if (digitalRead(ENCODER_CLK) == LOW) { 
+        
+        int dtValue = digitalRead(ENCODER_DT);
+        if (dtValue == LOW) encoder++;
+        else encoder--;
+        
+        lastEncoderReadTime = sekarang;
       }
     }
-    
-    delay(2); // Debounce delay
   }
-  
   lastCLK = currentCLK;
 }
 
 void setup() {
   RTC.begin();
-  Serial.begin(9600);
-  RTC.setHourMode(CLOCK_H12);
-
-  // Setup pins dengan pull-up internal
+  RTC.setHourMode(CLOCK_H24); 
+  
   pinMode(ENCODER_CLK, INPUT_PULLUP);
   pinMode(ENCODER_DT, INPUT_PULLUP);
   pinMode(ENCODER_BTN, INPUT_PULLUP);
-
-  // Baca initial state encoder
+  pinMode(BUZZER_PIN, OUTPUT); // Inisialisasi pin Buzzer
+  
   lastCLK = digitalRead(ENCODER_CLK);
 
   layarMAX.begin();
-  layarMAX.setIntensity(1);   // brightness
+  layarMAX.setIntensity(1);  
   layarMAX.displayClear();
-
-
-  // Initialize alarm values
-  jamAlarm = 6;
-  menitAlarm = 0;
-  onoff = true;
-
-  // Set alarm di RTC
-  RTC.setAlarm1(jamAlarm, menitAlarm, 0);
-  RTC.enableAlarm1();
-  RTC.enableAlarmPin();
-
-  Serial.println("=== DIGITAL CLOCK WITH ALARM ===");
-  Serial.println("Operasi:");
-  Serial.println("- CLICK: Ganti tampilan jam/tanggal");
-  Serial.println("- HOLD: Masuk/keluar setting alarm");
-  Serial.println("- PUTAR: Ubah nilai saat setting alarm");
-  Serial.println("===============================");
 }
 
 void loop() {
-  unsigned long sekarang = millis();
-  hours = RTC.getHours();
-  seconds = RTC.getSeconds();
-
-  // Update jam
-  jam();
-
-  // Cek alarm
-  if (RTC.isAlarm1Tiggered()) {
-    display = displayAlarm;
-    if (serialDebug) Serial.println(">>> ALARM BERBUNYI! Tekan tombol untuk matikan <<<");
-    
-    // Matikan alarm jika tombol ditekan
-    if (digitalRead(ENCODER_BTN) == LOW){
-      RTC.clearAlarm1();
-      display = displayJam;
-      if (serialDebug) Serial.println(">>> ALARM DIMATIKAN <<<");
-      delay(500); // Prevent bouncing
-    }
-  } 
-
-  // Baca input
-  readEncoderPolling();  // Gunakan polling method yang sudah bekerja
+  readEncoderPolling();
   readButton();
   readHandle();
-  tampilkanLayar();      // Tampilkan ke MAX7219
-
-  delay(50);
+  tampilkanLayar();
 }
